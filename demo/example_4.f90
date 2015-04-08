b25152e4b195f27fb7e6f1807c407d968d96c1be
Xiaoyu Wei <wxy0516@gmail.com>
2015-04-08 11:42:52 +0800

    Editing example_04

--- demo/example_4.f90
+++ demo/example_4.f90
@@ -4,12 +4,13 @@ Program example_4
   implicit none
   integer :: n=10,nnz=10
   integer :: ierr,i
-  double precision, allocatable, dimension(:) :: val,rhs
+  double precision, allocatable, dimension(:) :: val,rhs,init
   double precision, allocatable, dimension(:) :: solu
   integer, allocatable, dimension(:) :: col_ind,row_ptr
 
   allocate( val(nnz) )
   allocate( rhs(n) )
+  allocate( init(n) )
   allocate( solu(n) )
   allocate( col_ind(nnz) )
   allocate( row_ptr(n+1) )
@@ -22,14 +23,29 @@ Program example_4
   do i = 1,n
      rhs(i) = i**3
      row_ptr(i) = i
+     init(i) = 0.0
   end do
 
   row_ptr(n+1) = n+1  
 
+  write(*,*) "Matrix val:"
   write(*,*) val
+
+  write(*,*) "Matrix col_ind:"
+  write(*,*) col_ind
+
+  write(*,*) "Matrix row_ptr:"
+  write(*,*) row_ptr
+
+  write(*,*) "Right hand side:"
+  write(*,*) rhs
+
+  write(*,*) "Initial guess:"
+  write(*,*) init
+  
   write(*,*) "Hi, this is example 4, testing for linear solvers."
 
-  call arms_fgmres(n,val,col_ind,row_ptr,rhs,solu,ierr)
+  call arms_fgmres(n,val,col_ind,row_ptr,rhs,solu,init,ierr)
 
   write(*,*) "ierr = ",ierr,"."
   
